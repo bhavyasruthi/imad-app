f3815ae20a17cacf54eb20f9142b4ed93682de5c
bhavyasruthi <sbhavyasruthi@gmail.com>
2017-08-08 01:05:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,9 @@
 console.log('Loaded!');
 var ele= document.getElementById('text');
-
 ele.innerHTML = "its  new value !!!";
+//move img
+var img= document.getElementById('img');
+img.onclick = function()
+{
+    img.margin-top=200;
+};
