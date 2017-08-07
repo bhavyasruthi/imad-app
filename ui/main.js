834a5c1fdc2881f9e0d8efcef54227dd8c39f6ca
bhavyasruthi <sbhavyasruthi@gmail.com>
2017-08-08 02:43:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
 console.log('Loaded!');
+var counter=0;
+document.getElementById('b').onclick = function()
+{
+    alert("dfg");
+    counter++;
+    document.getElementById('b').innerHTML = counter;
+};
