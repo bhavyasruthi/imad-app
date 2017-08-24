9b5dbb9c31776328e7943dc525e3f77b2065f1f3
bhavyasruthi <sbhavyasruthi@gmail.com>
2017-08-24 22:37:12 +0530

    [imad-console] Updates ui/login.js

--- ui/login.js
+++ ui/login.js
@@ -23,5 +23,6 @@ submit.onclick = function()
             }
          };
    request.open('POST','http://sbhavyasruthi36.imad.hasura-app.io/login',true);
+   request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({"username":username ,"password" : password}));
 };
