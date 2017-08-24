90e99d3dfdafa72758ca0d14cfded6728c8b2ea5
bhavyasruthi <sbhavyasruthi@gmail.com>
2017-08-24 23:24:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,5 @@
-console.log('Loaded!');
+//main.js
+/*console.log('Loaded!');
 
 var submit= document.getElementById("submit");
 submit.onclick = function()
@@ -34,10 +35,40 @@ submit.onclick = function()
    request.open('GET','http://sbhavyasruthi36.imad.hasura-app.io/submitComment/:'+ncomment,true);
    //request.open('GET','http://sbhavyasruthi36.imad.hasura-app.io/submitComment/sru',true);
     request.send(null);
-};
+};*/
 /*submit.onclick = function(){
     console.log('in f!'+ncomment+"Szvsf");
     alert(ncomment);
      var namesList= document.getElementById("list");
    namesList.innerHTML= '<li>' + ncomment + '</li>';
 };*/
+//login.js
+console.log('loaded login.js');
+var submit= document.getElementById("submit");
+submit.onclick = function()
+  {
+      console.log('button clicked');
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    console.log('button clicked'+username+password);
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function()
+        {
+            console.log('in function readystatechnge!');
+        if(request.readyState === XMLHttpRequest.DONE)
+            {
+                console.log('in function liif readystten1!');
+            if(request.status === 200)
+                {
+                   alert("logged in") ;
+                }
+                else if(request.status === 403)
+                alert("Incorrect username & password");
+                 else if(request.status === 500)
+                alert("Something went wrong");
+            }
+         };
+   request.open('POST','http://sbhavyasruthi36.imad.hasura-app.io/login',true);
+   request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:username ,password : password}));
+};
